f9ae82d38081e2be6d44e88ce5010da598acc68d
Sredna Kunowski <srednakun@gmail.com>
2015-07-19 10:00:25 -0700

    fixed js

--- aboutMe.js
+++ aboutMe.js
@@ -1,5 +1,9 @@
-var aboutMeFolder = "aboutMePics";
+var aboutMeFolder = "./aboutMePics/";
 var fileExtenstion = ".JPG";
+var position = true;
+
+
+
 $.ajax({
     //This will retrieve the contents of the folder if the folder is configured as 'browsable'
     url: aboutMeFolder,
@@ -7,7 +11,24 @@ $.ajax({
         //Lsit all png file names in the page
         $(data).find("a:contains(" + fileExtenstion + ")").each(function () {
             var filename = this.href.replace(window.location.host, "").replace("http:///", "");
-            $("#leftPicsList").append($("<li><div style=\"background-image: url('" + filename + "')\" class=\"aboutMePics\"/></li>"));
+
+            if (position)
+            {
+            	$("#leftPicsList").append($("<li><div style=\"background-image: url('" + aboutMeFolder + filename + "')\" class=\"aboutMePics\"></div></li>"));
+                position = false;            
+            }
+            else
+            {
+            	$("#rightPicsList").append($("<li><div style=\"background-image: url('" + aboutMeFolder + filename + "')\" class=\"aboutMePics\"></div></li>"));
+ 				position = true;           
+            }
+
         });
-    }
+    },
+
+    error: function(xhr, textStatus, errorThrown)
+    {
+    	alert(errorThrown);
+	}
+    
 });
