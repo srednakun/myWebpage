219642665a86b72ab73d1cccbe4bad25e37900ad
Sredna Kunowski <srednakun@gmail.com>
2015-07-19 17:22:20 -0700

    latest update

--- funny.js
+++ funny.js
@@ -1,17 +1,19 @@
-var aboutMeFolder = "./aboutMePics/";
-var fileExtenstion = ".JPG";
-var position = true;
+var funnyFolder = "./funny/";
+var fileExtenstion = ".jpg";
+
 
 
 
 $.ajax({
     //This will retrieve the contents of the folder if the folder is configured as 'browsable'
-    url: aboutMeFolder,
+    url: funnyFolder,
     success: function (data) {
         //Lsit all png file names in the page
         $(data).find("a:contains(" + fileExtenstion + ")").each(function () {
-            var filename = this.href.replace(window.location.host, "").replace("http:///", "");
 
+            var filename = this.href.replace(window.location.host, "").replace("http:///", "");
+            alert(filename);
+/*
             if (position)
             {
             	$("#leftPicsList").append($("<li><div style=\"background-image: url('" + aboutMeFolder + filename + "')\" class=\"aboutMePics\"></div></li>"));
@@ -22,7 +24,7 @@ $.ajax({
             	$("#rightPicsList").append($("<li><div style=\"background-image: url('" + aboutMeFolder + filename + "')\" class=\"aboutMePics\"></div></li>"));
  				position = true;           
             }
-
+*/
         });
     },   
 });
